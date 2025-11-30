1e8f2bee3c3092121b097590acff4669cafbbff1
Léo <leogianfagna@gmail.com>
2025-11-30 15:49:14 -0300

    style: Margem do topo para sub capítulos

--- src/components/common/TopicTitle.jsx
+++ src/components/common/TopicTitle.jsx
@@ -6,7 +6,7 @@ export default function TopicTitle({ children }) {
       variant="h5"
       component="h1"
       gutterBottom
-      sx={{ fontWeight: 500 }}
+      sx={{ fontWeight: 500, mt: 6 }}
     >
       {children}
     </Typography>
