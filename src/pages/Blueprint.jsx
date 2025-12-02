892cf2b48ed78bc681bd5611537f56b9f2d0fa23
Léo <leogianfagna@gmail.com>
2025-12-02 20:32:13 -0300

    Ajustes no blueprint

--- src/pages/Blueprint.jsx
+++ src/pages/Blueprint.jsx
@@ -1,38 +1,24 @@
 import Title from "../components/common/Title";
 import Paragraph from "../components/common/Paragraph";
+import TopicTitle from "../components/common/TopicTitle";
+import Image from "../components/common/Image";
 
 export default function Blueprint() {
   return (
     <>
       <Title>Blueprint</Title>
-
       <Paragraph>
         Abaixo estão os diagramas desenvolvidos para cada fase do projeto.
       </Paragraph>
 
-      {/* FASE 1 */}
-      <Paragraph><strong>Fase 1 – Infraestrutura Básica</strong></Paragraph>
-      <img
-        src="/images/fase1.png"
-        alt="Diagrama da Fase 1"
-        style={{ width: "100%", maxWidth: "800px", marginBottom: "24px" }}
-      />
+      <TopicTitle>Fase 1 – Infraestrutura Básica</TopicTitle>
+      <Image src={"/src/assets/images/fase1.png"} alt={"Infraestrutura Básica"} width={800} />
 
-      {/* FASE 2 */}
-      <Paragraph><strong>Fase 2 – Core, VLANs e SDN</strong></Paragraph>
-      <img
-        src="/images/fase2.png"
-        alt="Diagrama da Fase 2"
-        style={{ width: "100%", maxWidth: "800px", marginBottom: "24px" }}
-      />
+      <TopicTitle>Fase 2 – Core, VLANs e SDN</TopicTitle>
+      <Image src={"/src/assets/images/fase2.png"} alt={"Core, VLANs e SDN"} width={800} />
 
-      {/* FASE 3 */}
-      <Paragraph><strong>Fase 3 – Traceroute e SNMP</strong></Paragraph>
-      <img
-        src="/images/fase3.png"
-        alt="Diagrama da Fase 3"
-        style={{ width: "100%", maxWidth: "800px" }}
-      />
+      <TopicTitle>Fase 3 – Traceroute e SNMP</TopicTitle>
+      <Image src={"/src/assets/images/fase3.png"} alt={"Traceroute e SNMP"} width={800} />
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,26 @@
 import Title from "../components/common/Title";
 import Paragraph from "../components/common/Paragraph";
+import SubTopic from "../components/common/SubTopic";
+import List from "../components/common/List";
+import ItemList from "../components/common/ItemList";
 
 export default function HomePage() {
   return (
     <>
       <Title>Projeto Douglas EFC04</Title>
       <Paragraph>
-        Página inicial da wiki do projeto.
-        Depois preencher com nome dos membros e o que mais a gente lembrar de importante.
+        Página inicial da wiki do projeto do <strong>GRUPO 4</strong>. Os
+        tópicos estão dividos nas páginas à esquerda.
       </Paragraph>
+
+      <SubTopic>Membros participantes</SubTopic>
+      <List>
+        <ItemList>LEONARDO FERRARO GIANFAGNA: 18174490</ItemList>
+        <ItemList>MATEUS COLFERAI MISTRO: 23002896</ItemList>
+        <ItemList>RAFAEL GONÇALVES MICHIELIN: 23012113</ItemList>
+        <ItemList>RENAN NEGRI CECOLIN: 23012651</ItemList>
+        <ItemList>SAMUEL ARANTES DO NASCIMENTO PRADO: 23013858</ItemList>
+      </List>
     </>
   );
 }
