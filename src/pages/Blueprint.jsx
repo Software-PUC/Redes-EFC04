c8de0837fc7aefaed7a923c26038643a3617129c
Léo <leogianfagna@gmail.com>
2025-12-02 23:06:57 -0300

    fix: Path das imagens blueprint

--- src/pages/Blueprint.jsx
+++ src/pages/Blueprint.jsx
@@ -3,6 +3,10 @@ import Paragraph from "../components/common/Paragraph";
 import TopicTitle from "../components/common/TopicTitle";
 import Image from "../components/common/Image";
 
+import fase1 from "/src/assets/images/fase1.png";
+import fase2 from "/src/assets/images/fase2.png";
+import fase3 from "/src/assets/images/fase3.png";
+
 export default function Blueprint() {
   return (
     <>
@@ -12,13 +16,13 @@ export default function Blueprint() {
       </Paragraph>
 
       <TopicTitle>Fase 1 – Infraestrutura Básica</TopicTitle>
-      <Image src={"/src/assets/images/fase1.png"} alt={"Infraestrutura Básica"} width={800} />
+      <Image src={fase1} alt={"Infraestrutura Básica"} width={800} />
 
       <TopicTitle>Fase 2 – Core, VLANs e SDN</TopicTitle>
-      <Image src={"/src/assets/images/fase2.png"} alt={"Core, VLANs e SDN"} width={800} />
+      <Image src={fase2} alt={"Core, VLANs e SDN"} width={800} />
 
       <TopicTitle>Fase 3 – Traceroute e SNMP</TopicTitle>
-      <Image src={"/src/assets/images/fase3.png"} alt={"Traceroute e SNMP"} width={800} />
+      <Image src={fase3} alt={"Traceroute e SNMP"} width={800} />
     </>
   );
 }
